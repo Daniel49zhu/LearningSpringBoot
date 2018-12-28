d870105d14ef6bda060afd78692bb3ce9749cb8e
Daniel49zhu <zhujiacheng94@163.com>
2018-12-28 22:52:29 +0800

    example9

---
+++ LearningSpring/src/main/java/example9/AwareConfig.java
@@ -0,0 +1,10 @@
+package example9;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("example9")
+public class AwareConfig {
+
+}
---
+++ LearningSpring/src/main/java/example9/Main.java
@@ -0,0 +1,17 @@
+package example9;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AwareConfig.class);
+
+        AwareService awareService = context.getBean(AwareService.class);
+
+        awareService.outputResult();
+
+        context.close();
+
+    }
+}
