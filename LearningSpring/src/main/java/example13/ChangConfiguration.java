152d3f13ebbed005ed57d72346450f68afde2276
Daniel49zhu <zhujiacheng94@163.com>
2018-12-28 23:56:00 +0800

    example13

---
+++ LearningSpring/src/main/java/example13/ChangConfiguration.java
@@ -0,0 +1,24 @@
+package example13;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+// 组合@Configuration元注解。
+@Configuration
+// 组合@ComponentScan元注解。
+@ComponentScan
+public @interface ChangConfiguration {
+
+    // 覆盖value参数。
+    String[] value() default {};
+}
---
+++ LearningSpring/src/main/java/example13/DemoService.java
@@ -0,0 +1,10 @@
+package example13;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    public void outputResult() {
+        System.out.println("从组合注解配置照样获得的bean");
+    }
+}
