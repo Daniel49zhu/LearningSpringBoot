7ff04c52ef228a7f2c2b04090a63cdcd4a8cc7f3
Daniel49zhu <zhujiacheng94@163.com>
2018-12-29 23:47:23 +0800

    example1

---
+++ LearningSpringMvc/src/main/java/example1/HelloController.java
@@ -0,0 +1,12 @@
+package example1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value="/index")
+    public String hello() {
+        return "index";
+    }
+}
