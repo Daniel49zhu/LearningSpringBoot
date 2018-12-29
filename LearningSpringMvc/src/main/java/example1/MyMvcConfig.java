d7f64f04935530b8b9b64d4e2689ffdb551439b8
Daniel49zhu <zhujiacheng94@163.com>
2018-12-29 22:18:41 +0800

    Spring MVC

---
+++ LearningSpringMvc/src/main/java/example1/MyMvcConfig.java
@@ -0,0 +1,24 @@
+package example1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@EnableScheduling
+@ComponentScan("example1")
+public class MyMvcConfig {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver =  new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/classes/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+}
