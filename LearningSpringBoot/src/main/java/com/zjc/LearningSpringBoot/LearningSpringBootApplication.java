83cdce279946aa76e5de9cc6f57fd7625a0d27ee
Daniel49zhu <zhujiacheng94@163.com>
2019-01-02 23:31:55 +0800

    spring boot

--- LearningSpringBoot/src/main/java/com/zjc/LearningSpringBoot/LearningSpringBootApplication.java
+++ LearningSpringBoot/src/main/java/com/zjc/LearningSpringBoot/LearningSpringBootApplication.java
@@ -1,5 +1,6 @@
 package com.zjc.LearningSpringBoot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class LearningSpringBootApplication {
+    //测试注入属性
+    @Value("${book.author}")
+    private String bookAuthor;
+    @Value("${book.name}")
+    private String bookName;
 
     @RequestMapping("/")
     String index() {
-        return "hello spring boot";
+        return "hello spring boot,book author:"+bookAuthor + " and book name: "+bookName ;
     }
 
 	public static void main(String[] args) {
