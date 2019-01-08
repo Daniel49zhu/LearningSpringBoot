f498df858606075a45d6c84ce04a3ef991592db0
Daniel49zhu <zhujiacheng94@163.com>
2019-01-08 21:37:18 +0800

    websocket

---
+++ SpringBootWeb/websocket/src/main/java/com/zjc/websocket/domain/WiselyMessage.java
@@ -0,0 +1,13 @@
+package com.zjc.websocket.domain;
+
+public class WiselyMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ SpringBootWeb/websocket/src/main/java/com/zjc/websocket/domain/WiselyResponse.java
@@ -0,0 +1,13 @@
+package com.zjc.websocket.domain;
+
+public class WiselyResponse {
+    private String responseMessage;
+
+    public WiselyResponse(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+}
