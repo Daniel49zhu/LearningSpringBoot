19d9ae58e1b58f663fda97d1cc9ca2ae25be88a3
Daniel49zhu <zhujiacheng94@163.com>
2019-01-09 22:30:08 +0800

    angularjs

--- SpringBootWeb/angularjs/src/main/java/com/zjc/websocket/WebsocketApplication.java
+++ SpringBootWeb/angularjs/src/main/java/com/zjc/websocket/WebsocketApplication.java
@@ -1,10 +1,21 @@
 package com.zjc.websocket;
 
+import com.zjc.websocket.domain.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class WebsocketApplication {
+
+
+	@RequestMapping(value = "/search",produces = {MediaType.APPLICATION_JSON_VALUE})
+	public Person search(String personName){
+		return new Person(personName,21,"chongqing");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
